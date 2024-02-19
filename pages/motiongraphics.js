a0f2e44c7601c022060dc41d91d5a7294df88c8a
Paudymac <paudymcateer@gmail.com>
2024-02-18 21:37:21 -0800

    updated global video styles

--- pages/motiongraphics.js
+++ pages/motiongraphics.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import utilStyles from '../styles/utils.module.css';
 import Layout, { siteTitle } from '../components/layout';
+import Image from 'next/image';
 
 export default function MotionGraphics({ allPostsData }) {
   return (
