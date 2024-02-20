284c09583a8158ee0e634a95057def9ca128e427
Paudymac <paudymcateer@gmail.com>
2024-02-19 18:23:32 -0800

    added new font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 import '../styles/global.css';
 
+import { Barlow } from 'next/font/google'
+ 
+const barlow = Barlow({
+  weight: '600',
+  subsets: ['latin'],
+})
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${barlow.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
