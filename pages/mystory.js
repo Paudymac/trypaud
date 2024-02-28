f5dcf7b9b1f4e05fdac6f3bded5b695bc0455ca8
Paudymac <paudymcateer@gmail.com>
2024-02-27 17:26:57 -0800

    Update mystory.js

--- pages/mystory.js
+++ pages/mystory.js
@@ -13,12 +13,12 @@ export default function Logos({ allPostsData }) {
       <div className={utilStyles.blog}>
         <h1>My Story</h1>
         <h3>Influences</h3>
-        <p>At University I fell in love with comedy of Bill Hicks. To me he was a kindred spirit. 
+        <p>At University I fell in love with the comedy of Bill Hicks. To me he was a kindred spirit. 
         I was devastated to find out he had already passed. I honestly feel if he was still around we’d be friends. 
         A conceit of mine I’ll admit. I consider Bill a prophet, speaking in the prophetic tradition of oppression 
         albeit through comedy rather than hymned platformed oratories like Martin Luther King. The stage was his pulpit. 
         <br /><br />
-        <i>A designer of hilarity sent back in time by the Universe to help the species evolve, a bit like the terminator but with jokes. </i> 
+        <i>I believe Bill was a designer of hilarity sent back in time by the Universe to help the species evolve, a bit like the terminator but with jokes. </i> 
         <br /><br />
         A hero of mine I consider his words nothing less than profound.
         Like all prophets he was never fully appreciated during his time on Gaia. Leaving us much too young at the age of 32. 
@@ -34,6 +34,10 @@ export default function Logos({ allPostsData }) {
         By observing his comedy over the span of his career, from when he first went on stage at the tender age of 15 to his final show when 
         he was dying of pancreatic cancer, you could, potentially say a tenant of Bill’s was, if he was a designer might be: if something worked could 
         it work better? If true update.</p>
+        <h3>Martial Arts</h3>
+        <h3>Vancouver</h3>
+        <h3>Stand Up</h3>
+        <h3>Moving Forward</h3>
         <a href="/">home</a>
         </div>
         </section>
