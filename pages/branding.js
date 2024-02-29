e3a7c879b8fc08e24a1fd9d4ea0b467a2530cb3f
Paudymac <paudymcateer@gmail.com>
2024-02-28 16:36:33 -0800

    added game manual

--- pages/branding.js
+++ pages/branding.js
@@ -11,10 +11,10 @@ export default function Logos({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <h1>Branding</h1>
+        <h3>Mechwarrior 5: Game Manual</h3>
+         <embed src="/images/branding/game-manual-latest.pdf" width="1280px" height="800px"  className={utilStyles.pdf}/>
         <h3>Smartt: Styleguide</h3>
         <embed src="/images/branding/Styleguide_PM_V6.pdf" width="1280px" height="800px"  className={utilStyles.pdf}/>
-        <h3>Mechwarrior 5: Game Manual</h3>
-        <h3>Mechwarrior 5: Technical Manual</h3>
         <br/>
         <a href="/">home</a>
         </section>
