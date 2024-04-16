4e323b044f9144f094258b8dd6ef23fc8d31a9a7
Paudymac <paudymcateer@gmail.com>
2024-04-16 08:59:53 -0700

    updated branding

--- pages/branding.js
+++ pages/branding.js
@@ -11,10 +11,18 @@ export default function Logos({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <h1>Branding</h1>
-        <h3>Mechwarrior 5: Game Manual</h3>
-         <embed src="/images/branding/game-manual-latest.pdf" width="1280px" height="800px"  className={utilStyles.pdf}/>
-        <h3>Smartt: Styleguide</h3>
-        <embed src="/images/branding/Styleguide_PM_V6.pdf" width="1280px" height="800px"  className={utilStyles.pdf}/>
+        <h3>Columbia College Brand Guide</h3>
+         <Image className={utilStyles.contentImg} src="/images/branding/Columbia_College_Brand_Guidelines_Final_Page_01.jpg" width={960} height={540} quality={100} alt="columbia college brand guide"/><br/>
+         <Image className={utilStyles.contentImg} src="/images/branding/Columbia_College_Brand_Guidelines_Final_Page_02.jpg" width={960} height={540} quality={100} alt="columbia college brand guide"/><br/>
+         <Image className={utilStyles.contentImg} src="/images/branding/Columbia_College_Brand_Guidelines_Final_Page_04.jpg" width={960} height={540} quality={100} alt="columbia college brand guide"/><br/>
+         <Image className={utilStyles.contentImg} src="/images/branding/Columbia_College_Brand_Guidelines_Final_Page_11.jpg" width={960} height={540} quality={100} alt="columbia college brand guide"/><br/>
+         <Image className={utilStyles.contentImg} src="/images/branding/Columbia_College_Brand_Guidelines_Final_Page_16.jpg" width={960} height={540} quality={100} alt="columbia college brand guide"/><br/>
+        <br/>
+        <h3>Sierra Wireless Ad</h3>
+         <Image className={utilStyles.contentImg} src="/images/branding/sierra-wireless-ad.jpg" width={960} height={540} quality={100} alt="sierra wireless ad"/><br/>
+        <br/>
+        <h3>Smartt Ad</h3>
+         <Image className={utilStyles.contentImg} src="/images/branding/smartt-ad.jpg" width={960} height={540} alt="smartt branded ad"/><br/>
         <br/>
         </section>
         <a href="/">&larr; home</a>
