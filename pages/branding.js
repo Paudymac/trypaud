035db1ea0aba95b9b1829f9ddbb20f879ba58853
Paudymac <paudymcateer@gmail.com>
2024-05-26 23:19:03 -0700

    updated story AI support

--- pages/branding.js
+++ pages/branding.js
@@ -11,35 +11,49 @@ export default function Logos({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <h1>Branding</h1>
-        <h3>Game Manual</h3>
+        <span className={utilStyles.datePortfolio}>2021</span>
+        <h3 className={utilStyles.header3Portfolio}>Game Manual / MW5 </h3>
+        <p className={utilStyles.descriptionPortfolio}>PGI [ Design / Development / Art Direction ]</p>
         <Image className={utilStyles.contentImg} src="/images/branding/Game Manual_v2_5_Page_01.jpg" width={960} height={540} quality={100} alt="MW5 Game Manual"/><br/>
         <Image className={utilStyles.contentImg} src="/images/branding/Game Manual_v2_5_Page_04.jpg" width={960} height={540} quality={100} alt="MW5 Game Manual"/><br/>
         <Image className={utilStyles.contentImg} src="/images/branding/Game Manual_v2_5_Page_08.jpg" width={960} height={540} quality={100} alt="MW5 Game Manual"/><br/>
         <Image className={utilStyles.contentImg} src="/images/branding/Game Manual_v2_5_Page_13.jpg" width={960} height={540} quality={100} alt="MW5 Game Manual"/><br/>
-        <h3>MW5 Technical Manual</h3>
+        <span className={utilStyles.datePortfolio}>2021</span>
+        <h3 className={utilStyles.header3Portfolio}>MW5 Technical Manual/ MW5</h3>
+        <p className={utilStyles.descriptionPortfolio}>PGI [ Design / Development / Art Direction ]</p>
         <Image className={utilStyles.contentImg} src="/images/branding/technical-manual-2024-v01_Page_01.jpg" width={960} height={540} quality={100} alt="MW5 Technical Manual"/><br/>
         <Image className={utilStyles.contentImg} src="/images/branding/technical-manual-2024-v01_Page_02.jpg" width={960} height={540} quality={100} alt="MW5 Technical Manual"/><br/>
         <Image className={utilStyles.contentImg} src="/images/branding/technical-manual-2024-v01_Page_66.jpg" width={960} height={540} quality={100} alt="MW5 Technical Manual"/><br/>
-        <h3>Piranha Games Social</h3>
+        <span className={utilStyles.datePortfolio}>2021</span>
+        <h3 className={utilStyles.header3Portfolio}>Piranha Games Social</h3>
+        <p className={utilStyles.descriptionPortfolio}>PGI [ Design / Development / Art Direction ]</p>
         <Image className={utilStyles.contentImg} src="/images/branding/social-mockup.png" width={960} height={540} quality={100} alt="PGI social"/><br/>
-        <h3>Columbia College Brand Guide</h3>
+        <span className={utilStyles.datePortfolio}>2012</span>
+        <h3 className={utilStyles.header3Portfolio}>Columbia College Brand Guide</h3>
+        <p className={utilStyles.descriptionPortfolio}>Smartt [ Design / Development / Art Direction ]</p>
          <Image className={utilStyles.contentImg} src="/images/branding/Columbia_College_Brand_Guidelines_Final_Page_01.jpg" width={960} height={540} quality={100} alt="columbia college brand guide"/><br/>
          <Image className={utilStyles.contentImg} src="/images/branding/Columbia_College_Brand_Guidelines_Final_Page_02.jpg" width={960} height={540} quality={100} alt="columbia college brand guide"/><br/>
          <Image className={utilStyles.contentImg} src="/images/branding/Columbia_College_Brand_Guidelines_Final_Page_04.jpg" width={960} height={540} quality={100} alt="columbia college brand guide"/><br/>
          <Image className={utilStyles.contentImg} src="/images/branding/Columbia_College_Brand_Guidelines_Final_Page_11.jpg" width={960} height={540} quality={100} alt="columbia college brand guide"/><br/>
          <Image className={utilStyles.contentImg} src="/images/branding/Columbia_College_Brand_Guidelines_Final_Page_16.jpg" width={960} height={540} quality={100} alt="columbia college brand guide"/><br/>
         <br/>
-        <h3>Sierra Wireless Ad</h3>
+        <span className={utilStyles.datePortfolio}>2012</span>
+        <h3 className={utilStyles.header3Portfolio}>Sierra Wireless Ad</h3>
+        <p className={utilStyles.descriptionPortfolio}>Smartt [ Design / Development / Art Direction ]</p>
          <Image className={utilStyles.contentImg} src="/images/branding/sierra-wireless-ad.jpg" width={960} height={540} quality={100} alt="sierra wireless ad"/><br/>
         <br/>
-        <h3>Smartt Branding</h3>
+        <span className={utilStyles.datePortfolio}>2012</span>
+        <h3 className={utilStyles.header3Portfolio}>Sierra Wireless Ad</h3>
+        <p className={utilStyles.descriptionPortfolio}>Smartt Branding [ Design / Development / Art Direction ]</p>
         <Image className={utilStyles.contentImg} src="/images/branding/Styleguide_PM_V6_Page_01.jpg" width={960} height={540} alt="smartt branded ad"/><br/>
         <Image className={utilStyles.contentImg} src="/images/branding/Styleguide_PM_V6_Page_04.jpg" width={960} height={540} alt="smartt branded ad"/><br/>
         <Image className={utilStyles.contentImg} src="/images/branding/Styleguide_PM_V6_Page_07.jpg" width={960} height={540} alt="smartt branded ad"/><br/>
          <Image className={utilStyles.contentImg} src="/images/branding/smartt-folder-front.jpg" width={960} height={540} alt="smartt branding"/><br/>
          <Image className={utilStyles.contentImg} src="/images/branding/smartt-folder.jpg" width={960} height={540} alt="smartt branded ad"/><br/>
-        <h3>Smartt Ad</h3>
-         <Image className={utilStyles.contentImg} src="/images/branding/smartt-ad.jpg" width={960} height={540} alt="smartt branded ad"/><br/>
+         <span className={utilStyles.datePortfolio}>2012</span>
+        <h3 className={utilStyles.header3Portfolio}>Smartt Ad</h3>
+        <p className={utilStyles.descriptionPortfolio}>Smartt Ad [ Design / Development / Art Direction ]</p>
+        <Image className={utilStyles.contentImg} src="/images/branding/smartt-ad.jpg" width={960} height={540} alt="smartt branded ad"/><br/>
         <br/>
         </section>
         <a href="/">&larr; home</a>
