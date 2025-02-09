95d590e4c48f750e8cabcbe02cfb5cd653a74d87
Paudymac <paudymcateer@gmail.com>
2025-02-09 01:16:08 -0800

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
+import styles from './layout.module.css';
 import Menu from './mobNav';
 import Link from 'next/link';
 import { useRouter } from 'next/router'; 
