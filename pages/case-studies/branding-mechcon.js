9ad492f6360001218f3441fdc1273cb2c389ec4c
Paudymac <paudymcateer@gmail.com>
2025-03-04 21:06:33 -0800

    updated case study 2
    - changed imgs
    - added more copy

--- pages/case-studies/branding-mechcon.js
+++ pages/case-studies/branding-mechcon.js
@@ -22,7 +22,7 @@ export default function CaseStudy({  }) {
           <div className={styles.caseStudyIntro}>
           <h1>Branding Mech_Con</h1>
           <p>
-          Mech_Con was a 2-day Gaming event celebrating the BattleTech game universe and Piranha MechWarrior games taking place in 2018. 
+          Mech_Con was a 2-day Gaming event celebrating the BattleTech game universe and Piranha Games MechWarrior titles taking place in 2018. 
           </p>
           <button onClick={scrollToTarget} className={`${stylesLayout.primaryBtn} ${styles.readBtn}`}>read</button>
           </div>
@@ -37,12 +37,12 @@ export default function CaseStudy({  }) {
     
         <article id="case-study">
         <div className={styles.caseStudyContainer}>
-          <h2>Mech_Con was a 2-day Gaming event celebrating the BattleTech game universe and Piranha MechWarrior games taking place in 2018. 
-          This case study is an exploration of the design system used for the creation of digital and print media branding Mech_Con.
+          <h2>Mech_Con was a 2-day Gaming event celebrating the BattleTech game universe and Piranha Games MechWarrior titles taking place in 2018. 
+          This case study is an exploration of the design system used for the creation of digital and print media branding of Mech_Con.
           </h2>
         <p>
         The event was an overwhelming success with gaming enthusiasts from around the world gathering to celebrate the BattleTech franchise and Mechs’. 
-        The task was to create a scalable brand for the event with touchpoints existing on screen in the form of a website, game streams, video, advertising, signs and a MechWarrior Online world series tournament.
+        The task was to create a scalable brand for the event with touchpoints existing on screen and physically in the form of a website, merch, game streams, video, advertising, signs and a MechWarrior Online world series tournament.
           </p>
           </div>
       
@@ -56,9 +56,10 @@ export default function CaseStudy({  }) {
                          />
 
 <div className={styles.caseStudyContainer}>
-  <p>The first step was to update the logo from Mech_Con 2017 and create a style-guide. 
-    I hit the sketch pad and ideated on a design which could be expanded, landing upon the shape of a hexagon, this dovetailed with the event setup using lots of rigging for lights and cameras and the BattleTech game which famously uses a hexagonal map with Mechs’ strategically placed for combat.</p>
-
+  <p>Step one involved updating the logo from Mech_Con 2017 and creating a style-guide. 
+    I hit the sketch pad and ideated on a design which could be expanded, landing upon the shape of a hexagon, 
+    this dovetailed with the event setup using rigging for lights and cameras and the BattleTech game which famously uses a hexagonal map with Mechs’ strategically placed for combat.</p>
+    <p>The website was created first for attendee’s to sign-up with print collateral developed in months preceding the event. The style-guide was shared between printers, the event staging firm and marketers allowing for a confluence of supplemental material to be organised with a consistent look and feel. </p>
     </div>
     <Image className={styles.caseStudyImg}
                            priority
@@ -68,6 +69,16 @@ export default function CaseStudy({  }) {
                            alt="Logo"
                          />
     <div className={styles.caseStudyContainer}>
+      <p>Bespoke player shirts were created for MechWarrior Online tournament teams outside of the Mech_Con brand. The shirts corresponded with the teams’ own logos and represented another design challenge in between the event and tournament branding. </p>
+      <p>A specially designed Mech was created by the Piranha Senior Concept Artist which was factored into the brand at a later point. The central use case for this unique Mech were the event custom tees.</p>
+      <p>The tournament branding while different vis-a-vis the event identity, using a fire orange as the primary colour, incorporated the hex in the form of a pattern representing the global player network of MechWarrior Online.</p>
+      <p> <Image className={styles.caseStudyImg}
+                           priority
+                           src="/images/case-studies/tournament-logo.webp"
+                           height={1080}
+                           width={1920}
+                           alt="Logo"
+                         /></p>
     <p className={styles.caseStudiesBtnContainer}>
         <a href="/case-studies" className={`${stylesLayout.primaryBtn} ${utilStyles.backToBtn}`}>&larr; Case Studies</a>
     </p>
