c8d0dfbed3be98f116f1e5ebf64e7cfc40873569
Paudymac <paudymcateer@gmail.com>
2025-03-10 21:23:50 -0700

    updated branding section

--- pages/branding.js
+++ pages/branding.js
@@ -4,6 +4,39 @@ import Layout, { siteTitle } from '../components/layout';
 import Image from 'next/image';
 import BackgroundImage from '../components/backgroundImage';
 import styles from '../components/backgroundImage.module.css';
+import ImageCarousel from '../components/ImageCarousel';
+
+const images = [
+        "/images/branding/mw5-dev-tee.webp",
+        "/images/branding/mw5-dev-tee2.webp",
+        "/images/branding/mw5-dev-tee-back.webp",
+      ];
+
+      const images1 = [
+        "/images/branding/Mechwarrior5-CLANS-game-manual-1.webp",
+        "/images/branding/Mechwarrior5-CLANS-game-manual-3.webp",
+       "/images/branding/Mechwarrior5-CLANS-game-manual-5.webp",
+      ];
+
+      const images2 = [
+        "/images/branding/GameManual_v2_5_Page_01.webp",
+        "/images/branding/GameManual_v2_5_Page_04.webp",
+       "/images/branding/GameManual_v2_5_Page_08.webp",
+       "/images/branding/GameManual_v2_5_Page_13.webp",
+      ];
+
+      const images3 = [
+        "/images/branding/technical-manual-2024-v01_Page_01.webp",
+        "/images/branding/technical-manual-2024-v01_Page_02.webp",
+       "/images/branding/technical-manual-2024-v01_Page_66.webp",
+      ];
+
+      const images4 = [
+        "/images/branding/Columbia_College_Brand_Guidelines_Final_Page_01.webp",
+        "/images/branding/Columbia_College_Brand_Guidelines_Final_Page_02.webp",
+        "/images/branding/Columbia_College_Brand_Guidelines_Final_Page_04.webp",
+        "/images/branding/Columbia_College_Brand_Guidelines_Final_Page_11.webp",
+      ];
 
 
 export default function Logos({ allPostsData }) {
@@ -18,32 +51,11 @@ export default function Logos({ allPostsData }) {
          <span className={utilStyles.datePortfolio}>2024</span>
         <h3 className={utilStyles.header3Portfolio}>MW5:Clans Game Manual</h3>
         <p className={utilStyles.descriptionPortfolio}> [ Design / Development ]</p>
-
         <div className={styles.imageBg}>
-        <BackgroundImage 
-        imageUrl="/images/branding/Mechwarrior5-CLANS-game-manual-1.webp"
-        // paddingBottom= "52%"
-        backgroundPositionY="center"
-        />
+        <ImageCarousel images={images1} />
         </div>
 
-        <div className={styles.imageBg}>
-        <BackgroundImage 
-        imageUrl="/images/branding/Mechwarrior5-CLANS-game-manual-3.webp"
-        // paddingBottom= "52%"
-        backgroundPositionY="center"
-        />
-        </div>
-
-        <div className={styles.imageBg}>
-        <BackgroundImage 
-        imageUrl="/images/branding/Mechwarrior5-CLANS-game-manual-5.webp"
-        // paddingBottom= "52%"
-        backgroundPositionY="center"
-        />
-        </div>
-
-        <span className={utilStyles.datePortfolio}>2024</span>
+        {/* <span className={utilStyles.datePortfolio}>2024</span>
         <h3 className={utilStyles.header3Portfolio}>MW5:Clans Technical Read Out</h3>
         <p className={utilStyles.descriptionPortfolio}> [ Design / Development ]</p>
 
@@ -53,74 +65,30 @@ export default function Logos({ allPostsData }) {
         // paddingBottom= "52%"
         backgroundPositionY="center"
         />
-        </div>
-
-       
+        </div> */}
 
         <span className={utilStyles.datePortfolio}>2021</span>
         <h3 className={utilStyles.header3Portfolio}>MW5:Mercs Game Manual</h3>
         <p className={utilStyles.descriptionPortfolio}> [ Design / Development / Art Direction ]</p>
-
         <div className={styles.imageBg}>
-        <BackgroundImage 
-        imageUrl="/images/branding/GameManual_v2_5_Page_01.webp"
-        // paddingBottom= "52%"
-        backgroundPositionY="center"
-        />
+        <ImageCarousel images={images2} />
         </div>
-
-         <div className={styles.imageBg}>
-        <BackgroundImage 
-        imageUrl="/images/branding/GameManual_v2_5_Page_04.webp"
-        // paddingBottom= "52%"
-        backgroundPositionY="center"
-        />
-        </div>
-
-         <div className={styles.imageBg}>
-        <BackgroundImage 
-        imageUrl="/images/branding/GameManual_v2_5_Page_08.webp"
-        // paddingBottom= "52%"
-        backgroundPositionY="center"
-        />
-        </div>
-
-        <div className={styles.imageBg}>
-        <BackgroundImage 
-        imageUrl="/images/branding/GameManual_v2_5_Page_13.webp"
-        // paddingBottom= "52%"
-        backgroundPositionY="center"
-        />
-        </div>
-
 
         <span className={utilStyles.datePortfolio}>2021</span>
         <h3 className={utilStyles.header3Portfolio}>MW5:Mercs Technical Manual</h3>
         <p className={utilStyles.descriptionPortfolio}> [ Design / Development / Art Direction ]</p>
-
         <div className={styles.imageBg}>
-        <BackgroundImage 
-        imageUrl="/images/branding/technical-manual-2024-v01_Page_01.webp"
-        // paddingBottom= "52%"
-        backgroundPositionY="center"
-        />
+        <ImageCarousel images={images3} />
         </div>
 
+
+        <span className={utilStyles.datePortfolio}>2020</span>
+        <h3 className={utilStyles.header3Portfolio}>Mechwarrior 5: Mercenaries Tee's</h3>
+        <p className={utilStyles.descriptionPortfolio}> [ Design / Print / Art Direction ]</p>
         <div className={styles.imageBg}>
-        <BackgroundImage 
-        imageUrl="/images/branding/technical-manual-2024-v01_Page_02.webp"
-        // paddingBottom= "52%"
-        backgroundPositionY="center"
-        />
+        <ImageCarousel images={images} />
         </div>
 
-        <div className={styles.imageBg}>
-        <BackgroundImage 
-        imageUrl="/images/branding/technical-manual-2024-v01_Page_66.webp"
-        // paddingBottom= "52%"
-        backgroundPositionY="center"
-        />
-        </div>
 
         <span className={utilStyles.datePortfolio}>2018</span>
         <h3 className={utilStyles.header3Portfolio}>Mechwarrior 5: Mercenaries Classic Box</h3>
@@ -281,45 +249,15 @@ export default function Logos({ allPostsData }) {
 
         <span className={utilStyles.datePortfolio}>2012</span>
         <h3 className={utilStyles.header3Portfolio}>Columbia College Brand Guide</h3>
-        <p className={utilStyles.descriptionPortfolio}>Smartt [ Design / Development / Art Direction ]</p>
-
+        <p className={utilStyles.descriptionPortfolio}>[ Design / Development / Art Direction ]</p>
         <div className={styles.imageBg}>
-        <BackgroundImage 
-        imageUrl="/images/branding/Columbia_College_Brand_Guidelines_Final_Page_01.webp"
-        paddingBottom= "77%"
-        backgroundPositionY="center"
-        />
-        </div>
-
-        <div className={styles.imageBg}>
-        <BackgroundImage 
-        imageUrl="/images/branding/Columbia_College_Brand_Guidelines_Final_Page_02.webp"
-        paddingBottom= "77%"
-        backgroundPositionY="center"
-        />
-        </div>
-
-        <div className={styles.imageBg}>
-        <BackgroundImage 
-        imageUrl="/images/branding/Columbia_College_Brand_Guidelines_Final_Page_04.webp"
-        paddingBottom= "77%"
-        backgroundPositionY="center"
-        />
-        </div>
-
-
-        <div className={styles.imageBg}>
-        <BackgroundImage 
-        imageUrl="/images/branding/Columbia_College_Brand_Guidelines_Final_Page_11.webp"
-        paddingBottom= "77%"
-        backgroundPositionY="center"
-        />
+        <ImageCarousel images={images4} />
         </div>
 
 
         <span className={utilStyles.datePortfolio}>2012</span>
         <h3 className={utilStyles.header3Portfolio}>Sierra Wireless Ad</h3>
-        <p className={utilStyles.descriptionPortfolio}>Smartt [ Design / Development / Art Direction ]</p>
+        <p className={utilStyles.descriptionPortfolio}> [ Design / Development / Art Direction ]</p>
 
 
         <div className={styles.imageBg}>
