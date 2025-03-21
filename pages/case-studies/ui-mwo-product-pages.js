3da969da0289e51e16105785e9152e08649bbda3
Paudymac <paudymcateer@gmail.com>
2025-03-21 13:57:22 -0700

    changed css order

--- pages/case-studies/ui-mwo-product-pages.js
+++ pages/case-studies/ui-mwo-product-pages.js
@@ -1,8 +1,8 @@
 import Head from 'next/head';
-import styles from '../../components/caseStudy.module.css';
 import stylesLayout from '../../components/layout.module.css';
 import Layout, { siteTitle } from '../../components/layout';
 import utilStyles from '../../styles/utils.module.css';
+import styles from '../../components/caseStudy.module.css';
 import Image from 'next/image';
 import scrollTo from '../../components/scrollTo';
 
