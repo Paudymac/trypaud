b236667af7125fb527e58e6095a24ebfe95b1a42
Paudymac <paudymcateer@gmail.com>
2025-04-19 12:23:56 -0700

    animated about me section on page load
    - added aboutme.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import Image from 'next/image';
 import ContactForm from '../components/contactForm';
 import styles from '../components/contactForm.module.css';
 import stylesLayout from '../components/layout.module.css';
@@ -9,7 +7,7 @@ import scrollTo from '../components/scrollTo';
 import PartnerLogos from '../components/partnerLogos';
 import Galleries from '../components/galleries';
 import CaseStudies from '../components/caseStudies';
-import Link from 'next/link';
+import AboutMe from '../components/aboutMe';
 
 
 export default function Home({ allPostsData }) {
@@ -54,24 +52,8 @@ export default function Home({ allPostsData }) {
 
           <CaseStudies />
 
-      <div className={utilStyles.aboutMeContainer}>
-      <h1>About Me</h1>
-      <div className={utilStyles.aboutMe}>
-      
-      <div className={utilStyles.aboutMeImgContainer}>
-      <div className={utilStyles.aboutMeImg}></div>
-      </div>
+          <AboutMe />
 
-        <div className={utilStyles.aboutMeDescipt}>
-          <h2 className={utilStyles.aboutMeHeadline}>Creativity & Design Good</h2>
-            <p className={utilStyles.aboutMeSummary}>I’ve always sketched and coloured from when I was small. Being creative is something I find innate. 
-              My 17 year design journey has allowed me to wear many hats concurrently, making me a versatile designer
-               with an eye for layout and identity mixed in with technical acumen through the lens of a user centered philosophy. I believe good design helps people. 
-               Like holding a door open and letting someone in. Great design keeps them coming back.</p>
-        </div>
-
-      </div>
-      </div>
       <div id="collaborate"></div>
       <div className={styles.contactFormContainer}>
           <h1>Let's Collaborate</h1>
